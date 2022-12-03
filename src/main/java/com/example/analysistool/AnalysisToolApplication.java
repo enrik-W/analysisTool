26267ad41c1bbcdbd98bfe8b7b6a8112bf1514b9
Henrik Wallerman <wallermanhenrik@gmail.com>
2022-12-03 11:39:23 +0100

    Initial commit

---
+++ src/main/java/com/example/analysistool/AnalysisToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.analysistool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalysisToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalysisToolApplication.class, args);
+    }
+
+}
